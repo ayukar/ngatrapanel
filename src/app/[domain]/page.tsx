b37efdec7afbc8c14812ea39c998619a2a72cfae
ayukar <ayunengtiaskarina@gmail.com>
2024-11-14 21:02:31 +0700

    feat:add params

--- src/app/[domain]/page.tsx
+++ src/app/[domain]/page.tsx
@@ -1,29 +1,22 @@
-import { auth } from "@/auth";
 import prisma from "@/lib/prisma";
-import Link from "next/link";
 import React from "react";
 import NotFound from "../not-found";
 
-type Props = {
-  domain: string;
-};
-
-const Site = async ({ domain }: Props) => {
+const Site = async ({ params }: { params: { domain: string } }) => {
+    const domain = params.domain
   const website = await prisma.website.findFirst({
     where: {
-        name : domain
+      name: domain,
     },
   });
-  if(!website){
-    return <NotFound/>
+  if (!website) {
+    return <NotFound />;
   }
   return (
     <div>
       <div className="text-xl">Selamat datang di {website?.name}</div>
       <div className="text-sm">websitenya :</div>
-      <div className="flex flex-col items-center justify-center text-sm text-blue-500">
-        
-      </div>
+      <div className="flex flex-col items-center justify-center text-sm text-blue-500"></div>
     </div>
   );
 };
