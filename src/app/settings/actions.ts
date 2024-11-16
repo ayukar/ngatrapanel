c84c90387a1bff4390841e899d944b5026f1a27c
JalalDA <jalaldalkhotami@gmail.com>
2024-11-16 15:39:41 +0700

    feat:delete user id

--- src/app/settings/actions.ts
+++ src/app/settings/actions.ts
@@ -17,14 +17,7 @@ export async function updateProfile(values: UpdateProfileValues) {
 
   const { name } = updateProfileSchema.parse(values);
 
-  await prisma.user.update({
-    where: {
-      id: userId,
-    },
-    data: {
-      name,
-    },
-  });
+
 
   revalidatePath("/");
 }
